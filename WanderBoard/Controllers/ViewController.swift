d412b5665cedf11d210192890f1a13700caef1b9
SijongKim93 <rlatlwhd456@gmail.com>
2024-05-28 21:11:31 +0900

    [Setting] 초기 세팅

---
+++ WanderBoard/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WanderBoard
+//
+//  Created by 김시종 on 5/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+        
+    }
+}
+

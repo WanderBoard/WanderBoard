31c650dc6a16ef912d2e427210f2932127af6e54
SijongKim93 <rlatlwhd456@gmail.com>
2024-06-08 00:04:24 +0900

    [Feat] 전체 앨범 보기 , 컬렉션 뷰 꾹 눌러 흔들고 개별 삭제 가능

--- WanderBoard/SijongKim93/GallaryInPutCollectionViewCell.swift
+++ WanderBoard/SijongKim93/GallaryInPutCollectionViewCell.swift
@@ -13,6 +13,14 @@ class GallaryInPutCollectionViewCell: UICollectionViewCell {
     let imageView = UIImageView()
     let addButton = UIButton(type: .system)
     
+    let deleteButton = UIButton(type: .custom).then {
+        $0.setImage(UIImage(systemName: "xmark")?.withTintColor(.white, renderingMode: .alwaysOriginal), for: .normal)
+        $0.backgroundColor = .red
+        $0.layer.cornerRadius = 12
+        $0.clipsToBounds = true
+        $0.isHidden = true
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -25,6 +33,7 @@ class GallaryInPutCollectionViewCell: UICollectionViewCell {
     func setupUI() {
         contentView.addSubview(imageView)
         contentView.addSubview(addButton)
+        contentView.addSubview(deleteButton)
         
         imageView.contentMode = .scaleAspectFill
         imageView.layer.cornerRadius = 16
@@ -49,9 +58,15 @@ class GallaryInPutCollectionViewCell: UICollectionViewCell {
             $0.edges.equalToSuperview()
         }
         
+        deleteButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(-8)
+            $0.trailing.equalToSuperview().offset(8)
+            $0.width.height.equalTo(24)
+        }
+        
+        deleteButton.addTarget(nil, action: #selector(DetailInputViewController.deletePhoto(_:)), for: .touchUpInside)
     }
-    
-    func configure(with image: UIImage?) {
+    func configure(with image: UIImage?, isEditing: Bool) {
         if let image = image {
             imageView.image = image
             imageView.isHidden = false
@@ -60,6 +75,29 @@ class GallaryInPutCollectionViewCell: UICollectionViewCell {
             imageView.isHidden = true
             addButton.isHidden = false
         }
+        showDeleteButton(isEditing)
+        if isEditing {
+            startShaking()
+        } else {
+            stopShaking()
+        }
     }
     
+    func showDeleteButton(_ show: Bool) {
+        deleteButton.isHidden = !show
+    }
+    
+    func startShaking() {
+        let animation = CABasicAnimation(keyPath: "transform.rotation")
+        animation.fromValue = -0.05
+        animation.toValue = 0.05
+        animation.duration = 0.1
+        animation.repeatCount = .greatestFiniteMagnitude
+        animation.autoreverses = true
+        layer.add(animation, forKey: "shake")
+    }
+    
+    func stopShaking() {
+        layer.removeAllAnimations()
+    }
 }

992363a9397dcbabbbbb4fc4d1da33837c6e61d4
pond1225 <pond1225@gmail.com>
2024-06-14 13:43:29 +0900

    [Fix] 네비게이션바 버튼 크기조정

--- WanderBoard/pond1225/NavigationBarFactory.swift
+++ WanderBoard/pond1225/NavigationBarFactory.swift
@@ -35,28 +35,31 @@ class NavigationBarFactory {
 class ButtonFactory {
     
     static func createXButton(target: Any?, action: Selector) -> UIButton {
-            return createButton(systemName: "x.circle", target: target, action: action)
+            return createButton(withImageName: "x.circle", target: target, action: action)
         }
         
         static func createPenButton(target: Any?, action: Selector) -> UIButton {
-            return createButton(systemName: "pencil.circle", target: target, action: action)
+            return createButton(withImageName: "pencil.circle", target: target, action: action)
         }
         
         static func createBackButton(target: Any?, action: Selector) -> UIButton {
-            return createButton(systemName: "lessthan.circle", target: target, action: action)
+            return createButton(withImageName: "lessthan.circle", target: target, action: action)
         }
         
         static func createSaveButton(target: Any?, action: Selector) -> UIButton {
-            return createButton(systemName: "square.and.arrow.down", target: target, action: action)
+            return createButton(withImageName: "square.and.arrow.down", target: target, action: action)
         }
         
         static func createPinButton(target: Any?, action: Selector) -> UIButton {
-            return createButton(systemName: "pin.circle", target: target, action: action)
+            return createButton(withImageName: "pin.circle", target: target, action: action)
         }
         
-        private static func createButton(systemName: String, target: Any?, action: Selector) -> UIButton {
+        private static func createButton(withImageName imageName: String, target: Any?, action: Selector) -> UIButton {
             let button = UIButton()
-            button.setImage(UIImage(systemName: systemName), for: .normal)
+            let imageConfig = UIImage.SymbolConfiguration(pointSize: 25, weight: .light)
+            let image = UIImage(systemName: imageName, withConfiguration: imageConfig)
+            
+            button.setImage(image, for: .normal)
             button.tintColor = .black
             button.addTarget(target, action: action, for: .touchUpInside)
             return button

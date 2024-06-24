fedbbeb5f738831878b342aee46304e4e8de15de
mgynsz <mgynsz@gmail.com>
2024-06-24 12:54:30 +0900

    repair app,scenedelegate for guestmembers

--- WanderBoard/SceneDelegate.swift
+++ WanderBoard/SceneDelegate.swift
@@ -31,7 +31,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             window?.makeKeyAndVisible()
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                self.window?.rootViewController = AuthenticationVC()
+                self.window?.rootViewController = PageViewController()
                 self.window?.makeKeyAndVisible()
             }
             return
